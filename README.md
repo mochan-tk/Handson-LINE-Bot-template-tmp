cc9179894dd4686ad376631f6c337465891212b5
mochan_tk <takashi1.kawamoto@gmail.com>
2022-12-12 09:46:15 +0000

    commit

--- README.md
+++ README.md
@@ -4,42 +4,7 @@
 ## command
 
 ```
-az upgrade
-az bicep install
-
-az login --use-device-code
-
- az account list \
-   --refresh \
-   --query "[].id" \
-   --output table
-
-az account set --subscription <your subscription id>
-
-group_name=20211027HappyHackLineBot
-
-az group create --name ${group_name} --location japaneast
-
-az configure --defaults group=${group_name}
-
-## https://1password.com/jp/password-generator/
-az deployment group create --name deployPrj01 --template-file main.bicep \
-  --parameters ramdom=<ramdom> \
-  --parameters secret=<secret> \
-  --parameters access=<access>
-  
-az deployment group show \
-  -g ${group_name} \
-  -n deployPrj01 \
-  --query properties.outputs.functionAppName.value
-
-cd deployPrj01
-npm i
-func azure functionapp publish <functionAppName> --build remote
-
-#az group delete --name ${group_name}
+npm install
+npm run start
 ```
 
-##
-
-https://azure.github.io/AppService/2021/07/23/Quickstart-Intro-to-Bicep-with-Web-App-plus-DB.html
---
+++ index.js
@@ -0,0 +1,371 @@
+const line = require('@line/bot-sdk');
+const express = require("express")
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000
+// base URL for webhook server
+let baseURL = process.env.BASE_URL;
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// serve static and downloaded files
+app.use('/downloaded', express.static('downloaded'));
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', line.middleware(config), async (req, res) => {
+  Promise
+    .all(req.body.events.map(await handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+//app.post("/webhook", function(req, res) {
+async function handleEvent(event) {
+  const userId = event.source.userId;
+  if (event.type !== 'message' && event.type !== 'postback') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  } else if (event.type === 'postback') {
+    if (event.postback.data === 'sticker') {
+      //https://developers.line.biz/ja/reference/messaging-api/#sticker-message
+      //https://developers.line.biz/ja/docs/messaging-api/sticker-list/#sticker-definitions
+      return client.replyMessage(event.replyToken,{
+        type: 'sticker',
+        packageId: "11537",
+        stickerId: "52002735"
+      });
+    }
+  
+  } else if (event.message.type === 'text') {
+    if (event.message.text === 'flex') {
+      //https://developers.line.biz/ja/reference/messaging-api/#flex-message
+      return client.replyMessage(event.replyToken,{
+        type: 'flex',
+        altText: 'item list',
+        contents: flexMsg
+      });
+    } else if (event.message.text === 'quick') {
+      //https://developers.line.biz/ja/reference/messaging-api/#quick-reply
+      return client.replyMessage(event.replyToken,{
+        type: 'text',
+        text: 'ステッカー欲しいですか❓YesかNoで答えてください, もしくは素敵な写真送って❗️',
+        "quickReply": {
+          "items": [
+            {
+              "type": "action",
+              "action": {
+                "type":"postback",
+                "label":"Yes",
+                "data": "sticker",
+                "displayText":"ステッカーください❗️"
+              }
+            },
+            {
+              "type": "action",
+              "action": {
+                "type":"message",
+                "label":"No",
+                "text":"不要。"
+              }
+            },
+            {
+              "type": "action",
+              "action": {
+                "type": "camera",
+                "label": "camera"
+              }
+            }
+          ]
+        }
+      });
+    }
+
+  } else if (event.message.type === 'image') {
+    //https://developers.line.biz/ja/reference/messaging-api/#image-message
+    await handleImage(event.message, event.replyToken);
+  } else if (event.message.type === 'audio') {
+    //https://developers.line.biz/ja/reference/messaging-api/#audio-message
+    await handleAudio(event.message, event.replyToken);
+  } else if (event.message.type === 'location') {
+    //https://developers.line.biz/ja/reference/messaging-api/#location-message
+    return client.replyMessage(event.replyToken,{
+      type: 'location',
+      title: 'my location',
+      address: event.message.address,
+      latitude: event.message.latitude,
+      longitude: event.message.longitude
+    });
+  }
+  const echo = { type: 'text', text: event.message.text };
+  return client.replyMessage(event.replyToken, echo);
+}
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
+
+async function handleImage(message, replyToken) {
+  let getContent;
+  if (message.contentProvider.type === "line") {
+    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.jpg`);
+    const previewPath = path.join(__dirname, 'downloaded', `${message.id}-preview.jpg`);
+
+    getContent = downloadContent(message.id, downloadPath)
+      .then((downloadPath) => {
+        // ImageMagick is needed here to run 'convert'
+        // Please consider about security and performance by yourself
+        //cp.execSync(`convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`);
+
+        return {
+          originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+          previewImageUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+        };
+      });
+  } else if (message.contentProvider.type === "external") {
+    getContent = Promise.resolve(message.contentProvider);
+  }
+
+  return getContent
+    .then(({ originalContentUrl, previewImageUrl }) => {
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'image',
+          originalContentUrl,
+          previewImageUrl,
+        }
+      );
+    });
+}
+
+async function handleAudio(message, replyToken) {
+  let getContent;
+  if (message.contentProvider.type === "line") {
+    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.m4a`);
+
+    getContent = downloadContent(message.id, downloadPath)
+      .then((downloadPath) => {
+        return {
+            originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+        };
+      });
+  } else {
+    getContent = Promise.resolve(message.contentProvider);
+  }
+
+  return getContent
+    .then(({ originalContentUrl }) => {
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'audio',
+          originalContentUrl,
+          duration: message.duration,
+        }
+      );
+    });
+}
+
+function downloadContent(messageId, downloadPath) {
+  return client.getMessageContent(messageId)
+    .then((stream) => new Promise((resolve, reject) => {
+      const writable = fs.createWriteStream(downloadPath);
+      stream.pipe(writable);
+      stream.on('end', () => resolve(downloadPath));
+      stream.on('error', reject);
+    }));
+}
+
+const flexMsg = {
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Arm Chair, White",
+            "wrap": true,
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "text",
+                "text": "$49",
+                "wrap": true,
+                "weight": "bold",
+                "size": "xl",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": ".99",
+                "wrap": true,
+                "weight": "bold",
+                "size": "sm",
+                "flex": 0
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Add to Cart",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "uri",
+              "label": "Add to wishlist",
+              "uri": "https://linecorp.com"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Metal Desk Lamp",
+            "wrap": true,
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "flex": 1,
+            "contents": [
+              {
+                "type": "text",
+                "text": "$11",
+                "wrap": true,
+                "weight": "bold",
+                "size": "xl",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": ".99",
+                "wrap": true,
+                "weight": "bold",
+                "size": "sm",
+                "flex": 0
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "text": "Temporarily out of stock",
+            "wrap": true,
+            "size": "xxs",
+            "margin": "md",
+            "color": "#ff5551",
+            "flex": 0
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "flex": 2,
+            "style": "primary",
+            "color": "#aaaaaa",
+            "action": {
+              "type": "uri",
+              "label": "Add to Cart",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "uri",
+              "label": "Add to wish list",
+              "uri": "https://linecorp.com"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "flex": 1,
+            "gravity": "center",
+            "action": {
+              "type": "uri",
+              "label": "See more",
+              "uri": "https://linecorp.com"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
